1c1941284d6f66d7dd330837e7ace9df91a3bf3e
mrjackinc <mrjackinc@gmail.com>
2016-09-28 22:34:30 -0700

    initial commit

---
+++ Assets/Code/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject StarMap;
+	bool StarMapToggle = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		StarMap.SetActive(StarMapToggle);
+	}
+
+	public void OnButtonClick()
+	{
+		StarMapToggle = !StarMapToggle;
+	}
+}
