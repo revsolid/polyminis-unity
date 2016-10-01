f7669a8b345f36dcceb959a2d46f7af2a89f34a4
mrjackinc <mrjackinc@gmail.com>
2016-09-30 22:42:36 -0700

    Almost there

--- Assets/Code/SpaceExploration/SpaceMovementTracker.cs
+++ Assets/Code/SpaceExploration/SpaceMovementTracker.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class SpaceMovementTracker : MonoBehaviour
 {
-	public GameObject SpaceMaterial;
+	public GameObject SpaceSphere1;
+	public GameObject SpaceSphere2;
+	public GameObject HUD;
 
 	// Use this for initialization
 	void Start ()
@@ -16,6 +18,11 @@ public class SpaceMovementTracker : MonoBehaviour
 	{
 		float horImpulse = Input.GetAxis ("Horizontal");
 		float verImpulse = Input.GetAxis ("Vertical");
+		SpaceSphere1.transform.Rotate(new Vector3(-1*verImpulse * 0.01f, horImpulse * 0.1f, 0.0f));
+		SpaceSphere2.transform.Rotate(new Vector3(verImpulse * 0.1f, -1*horImpulse * 0.01f, 0.0f));
+
+		// Rotate the Hud a bit.
+		HUD.transform.eulerAngles = new Vector3(0.0f, 0.0f, -1*horImpulse);
 
 	}
 }
