b06b30c4b40d6f636dd517a6e6ebe19bb9a49778
mrjackinc <mrjackinc@gmail.com>
2016-10-18 13:44:04 -0700

    Creature building and movement streaming

--- Assets/Code/CreatureObservation/Creature.cs
+++ Assets/Code/CreatureObservation/Creature.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Creature : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Creature : MonoBehaviour
+{
+	public Organelle OrganellePrototype;
 	
+	Dictionary<Vector2, int> OrganelleMap;
+	
+	void Awake()
+	{
+		OrganelleMap = new Dictionary <Vector2, int> ()
+		{
+			{ new Vector2(0,0), 114 },
+			{ new Vector2(1,0), 201 },
+			{ new Vector2(1,1), 85 },
+			{ new Vector2(2,0), 182 },
+		};
+	}
+	
+	void Start ()
+	{
+		foreach(KeyValuePair<Vector2, int> entry in OrganelleMap)
+		{
+			Vector2 delta = entry.Key;	
+			int colorOfset = entry.Value;
+			
+			Organelle o = GameObject.Instantiate(OrganellePrototype);
+			o.transform.parent = transform;
+			delta *= 2.5f;
+			o.transform.localPosition += new Vector3(delta.x, 0.0f, delta.y);
+			o.OrganelleModel = new OrganelleModel(colorOfset);
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
 	}
 }
--- Assets/Code/CreatureObservation/CreatureMover.cs
+++ Assets/Code/CreatureObservation/CreatureMover.cs
@@ -1,40 +1,130 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
+
+//TMP
+enum MovementType
+{
+    ROTATE,
+    TRANSLATE
+}
+
+class CreatureMovementAction
+{
+    public MovementType MovType;
+    public Transform Target;
+    public Quaternion TargetRotation;
+	public float TimeStarted;
+	
+	public CreatureMovementAction(MovementType mType, Transform target, Quaternion targetRotation)
+	{
+		MovType = mType;
+		Target = target;
+		TargetRotation = targetRotation;
+		if (!Validate())	
+		{
+			// ERROR
+		}
+	}
+	
+	public bool Validate()
+	{
+		switch (MovType)
+		{
+            case MovementType.TRANSLATE:
+				return Target != null;
+            case MovementType.ROTATE:
+				return TargetRotation != Quaternion.identity;
+		}
+		return false;
+	}
+	
+	public void Start()
+	{
+		TimeStarted = Time.time;
+	}
+}
+
 
 public class CreatureMover : MonoBehaviour
 {
 
-	public Transform CurrentTarget;
-	public float Speed;
-	
-	Quaternion TargetRotation = RIGHT_HEADING;
-	
-	enum Headings
-	{
-		Up,
-		Down,
-		Left,
-		Right,
-	}
-	
-	static Quaternion UP_HEADING = Quaternion.Euler(0, 0, 0);
-	static Quaternion DOWN_HEADING = Quaternion.Euler(0, 180, 0);
-	static Quaternion LEFT_HEADING = Quaternion.Euler(0, -90, 0);
-	static Quaternion RIGHT_HEADING = Quaternion.Euler(0, 90, 0);
+    public float Speed;
+    public Transform CurrentTarget;
+    Quaternion TargetRotation = RIGHT_HEADING;
+    
+    CreatureMovementAction CurrentAction;
+    Queue<CreatureMovementAction> ActionStream;
+    
+    enum Headings
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+    
+    static Quaternion UP_HEADING = Quaternion.Euler(0, 0, 0);
+    static Quaternion DOWN_HEADING = Quaternion.Euler(0, 180, 0);
+    static Quaternion LEFT_HEADING = Quaternion.Euler(0, -90, 0);
+    static Quaternion RIGHT_HEADING = Quaternion.Euler(0, 90, 0);
 
-	void Start ()
-	{ 
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		//TODO: Either by animation or some interesting math make it look like "impulse and decay" instead
-		//      of just linear advancing
-		Vector3 azimuth = gameObject.transform.position - CurrentTarget.position; 
-		azimuth.Normalize();
-		gameObject.transform.Translate(-1 * azimuth * Time.deltaTime * Speed, Space.World);
+    void Start ()
+    { 
+        ActionStream = new Queue<CreatureMovementAction>();
+		ActionStream.Enqueue(new CreatureMovementAction(MovementType.ROTATE, null, RIGHT_HEADING));
+		ActionStream.Enqueue(new CreatureMovementAction(MovementType.ROTATE, null, UP_HEADING));
+		ActionStream.Enqueue(new CreatureMovementAction(MovementType.ROTATE, null, RIGHT_HEADING));
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
 		
-		gameObject.transform.localRotation = Quaternion.Slerp(gameObject.transform.localRotation, TargetRotation, Time.time * Speed);
-	}
+        if (CurrentAction == null)
+        {
+			if (ActionStream.Count > 0)
+			{
+				CurrentAction = ActionStream.Dequeue();
+				CurrentAction.Start();
+			}
+			else
+			{
+            	return;
+			}
+        }
+        
+        switch (CurrentAction.MovType)
+        {
+            case MovementType.TRANSLATE:
+                //TODO: Either by animation or some interesting math make it look like "impulse and decay" instead
+                //      of just linear advancing
+                Vector3 azimuth = gameObject.transform.position - CurrentAction.Target.position; 
+				Debug.Log(azimuth.sqrMagnitude);
+				if (azimuth.sqrMagnitude < 0.001)
+				{
+					gameObject.transform.position = CurrentAction.Target.position;
+					CurrentAction = null;
+				}
+				else
+				{
+                	azimuth.Normalize();
+                	gameObject.transform.Translate(-1 * azimuth * Time.deltaTime * Speed, Space.World);
+				}
+                break;
+                
+            case MovementType.ROTATE:
+				if (Quaternion.Angle(gameObject.transform.localRotation, CurrentAction.TargetRotation) < 0.01)
+				{
+					CurrentAction = null;
+				}
+				else
+				{
+					float t = Time.time - CurrentAction.TimeStarted;
+    	            gameObject.transform.localRotation = Quaternion.Slerp(gameObject.transform.localRotation, CurrentAction.TargetRotation, t * Speed / 8);
+				}
+                break;
+        }
+    }
 }
