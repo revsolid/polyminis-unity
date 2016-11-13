2a45fa9b6600ffe223821a04d3ba602df2cca9d9
Rastikko <rastikko@gmail.com>
2016-11-12 21:16:26 -0800

    Adding all the file changes

---
+++ Assets/Code/SpaceExploration/OrbitalUIWidget.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OrbitalUIWidget : MonoBehaviour {
+
+    public Text text;
+
+    // Use this for initialization
+    void Start () {
+        RectTransform rt = GetComponent<RectTransform>();
+        float offsetX = (rt.localRotation.z > 0) ? 2f : -2f;
+        Vector3 textPosition = new Vector3(transform.position.x + offsetX, transform.position.y + 2f, transform.position.z);
+        text.transform.position = textPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
