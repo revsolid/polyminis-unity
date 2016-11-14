6b033c79b1a0234a9c3e3eecd20534cb851f6117
Rastikko <rastikko@gmail.com>
2016-11-13 17:00:50 -0800

    Use onWillRender instead of start

--- Assets/Code/SpaceExploration/OrbitalUIWidget.cs
+++ Assets/Code/SpaceExploration/OrbitalUIWidget.cs
@@ -6,16 +6,14 @@ public class OrbitalUIWidget : MonoBehaviour {
 
     public Text text;
 
-    // Use this for initialization
-    void Start () {
-        RectTransform rt = GetComponent<RectTransform>();
-        float offsetX = (rt.localRotation.z > 0) ? 2f : -2f;
-        Vector3 textPosition = new Vector3(transform.position.x + offsetX, transform.position.y + 2f, transform.position.z);
-        text.transform.position = textPosition;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnWillRenderObject()
+    {
+        if (text != null)
+        {
+            RectTransform rt = GetComponent<RectTransform>();
+            float offsetX = (rt.localRotation.z > 0) ? 2f : -2f;
+            Vector3 textPosition = new Vector3(transform.position.x + offsetX, transform.position.y + 2f, transform.position.z);
+            text.transform.position = textPosition;
+        }
+    }
 }
