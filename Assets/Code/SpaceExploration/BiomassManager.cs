92efdf2f1cd3f90d4c041216c18fc9e675c3068e
Jack <jjelliot@usc.edu>
2016-12-05 18:21:28 -0800

    Added code for Biomass implementation

---
+++ Assets/Code/SpaceExploration/BiomassManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class BiomassManager : MonoBehaviour {
+
+    public OrbitalUI Orbital;
+    public Text BiomassCounter;
+    static int Biomass;
+
+
+    public static void ConsumeBiomass(int amount)
+    {
+        if (amount <= Biomass)
+        {
+            Biomass -= amount;
+        }
+    }
+
+    public static void AddBiomass(int amount)
+    {
+        Biomass += amount;
+    }
+
+    public void Update()
+    {
+        BiomassCounter.text = "" + Biomass;
+    }
+
+    public static bool HasBiomass(int amount)
+    {
+        return amount <= Biomass;
+    }
+    
+}
--- Assets/Code/SpaceExploration/SpeciesCard.cs
+++ Assets/Code/SpaceExploration/SpeciesCard.cs
@@ -1,18 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SpeciesCard : MonoBehaviour
 {
-
+    public GameObject SpeciesMenu, ExtractionMenu;
     GameObject OrbitalUI;
+    int AmmountToExtract = 0;
+    private SpeciesModel Species;
+    public Text IndicatiorText;
+    public BiomassManager PlayerMgr;
+
+
 	// Use this for initialization
 	void Start ()
     {
+        ExtractionMenu.SetActive(false);
+        SpeciesMenu.SetActive(false);
         OrbitalUI = GameObject.FindGameObjectWithTag("OrbitalUI");
 	}
 
     public void OnClicked()
     {
+        //OrbitalUI.GetComponent<OrbitalUI>().OnEditCreatureClicked("Cool dudes");
+        SpeciesMenu.SetActive(!SpeciesMenu.activeInHierarchy);
+        if (ExtractionMenu.activeInHierarchy) ExtractionMenu.SetActive(false);
+
+    }
+
+    public void OpenExtractMenu()
+    {
+        ExtractionMenu.SetActive(!ExtractionMenu.activeInHierarchy);
+    }
+
+    public void OpenEditor()
+    {
         OrbitalUI.GetComponent<OrbitalUI>().OnEditCreatureClicked("Cool dudes");
+    }
+
+    public void IncrementAmmount()
+    {
+        AmmountToExtract++;
+        IndicatiorText.text = ""+AmmountToExtract;
+   }
+
+    public void DecrementAmmount()
+    {
+        AmmountToExtract--;
+        IndicatiorText.text = "" + AmmountToExtract;
+    }
+
+    public void SetAmmount(int i)
+    {
+        AmmountToExtract = i;
+    }
+
+    public void Extract()
+    {
+        //TODO: Implelent Extraction from planet simulation
+        BiomassManager.AddBiomass(AmmountToExtract);
+        AmmountToExtract = 0;
+        IndicatiorText.text = "" + AmmountToExtract;
     }
 }
