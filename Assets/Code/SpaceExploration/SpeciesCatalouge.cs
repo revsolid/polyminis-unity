b8a4a88fe4d69d006cc8b0b7632248ac9569f8eb
Jack <jjelliot@usc.edu>
2016-12-06 12:31:47 -0800

    Added UI Elements and code Skeleton for Species catalouge

---
+++ Assets/Code/SpaceExploration/SpeciesCatalouge.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpeciesCatalouge : MonoBehaviour {
+
+	VerticalLayoutGroup SpeciesGroup;
+
+	void Start () {
+	    
+	}
+	
+	
+	void Update () {
+
+	}
+
+    public void OnCLoseButtonClicked()
+    {
+        gameObject.SetActive(false);
+    }
+}
