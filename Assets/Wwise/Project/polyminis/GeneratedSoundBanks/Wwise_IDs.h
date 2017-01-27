a944b24350a41d0f42c5b3a71f7c43d710fc4cba
mrjackinc <mrjackinc@gmail.com>
2017-01-26 18:21:00 -0800

    Built / Cleaned up banks from Ernesto

--- Assets/Wwise/Project/polyminis/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/Wwise/Project/polyminis/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,11 +13,20 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID P_UI_ACCEPTED = 2187412920U;
+        static const AkUniqueID P_UI_ADD = 4094348196U;
+        static const AkUniqueID P_UI_CANCEL = 2258750257U;
+        static const AkUniqueID P_UI_DATA = 2903831199U;
+        static const AkUniqueID P_UI_ERROR = 3781701507U;
+        static const AkUniqueID P_UI_OK = 2141736205U;
+        static const AkUniqueID P_UI_RETURN = 2581921037U;
+        static const AkUniqueID P_UI_SUBSTRACT = 2374130668U;
+        static const AkUniqueID P_UI_WARNING = 1935057997U;
+        static const AkUniqueID P_WOOSH = 4058515758U;
         static const AkUniqueID PLAY_CREATURE_OBSERVATION = 369775550U;
         static const AkUniqueID PLAY_EXPLORATION = 2287072429U;
         static const AkUniqueID PLAY_INTRO_THEME = 2290104414U;
         static const AkUniqueID PLAY_SPECIES_DESIGN = 585562449U;
-        static const AkUniqueID PLAY_WHOOSH_1 = 4268550160U;
     } // namespace EVENTS
 
     namespace BANKS
