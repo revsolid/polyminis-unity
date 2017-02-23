873696d00f0c4b181ecafa19d83ef851f6b3159d
elseniorlopez <info@carlosms.com>
2017-02-22 23:53:08 -0600

    Audio Stuff
    *Created Audio Busses for SFX and Music
    *Created an AutoDucking feature so when SFX Plays, Music lowers itself
    -9dbb
    *Rearranged SFX and Music Bank contents
    *Changed file settings for music to be streamed insteado of added
    directly to de soundbank to lower loading times

--- Assets/Wwise/Project/polyminis/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/Wwise/Project/polyminis/GeneratedSoundBanks/Wwise_IDs.h
@@ -38,8 +38,12 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID INGAME = 984691642U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
         static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BUSSES
 
 }// namespace AK
