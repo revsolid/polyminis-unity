c6f43e1ca7bfbc7dcda217bafa64e825872ee4b5
Yanqi Roy Niu (PC) <yanqi.roy.niu@gmail.com>
2017-03-20 17:46:00 -0700

    PlanetModel passed into creature_obs scene.

---
+++ Assets/Code/CreatureObservation/PlanetInfoCacher.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is used to carry PlanetModel into creature_obs scene.
+/// </summary>
+public class PlanetInfoCacher
+{
+    public static PlanetModel planetModel;
+}
