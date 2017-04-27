e33185bda1b802a7c335b9a2cb71373a1444c60b
mrjackinc <mrjackinc@gmail.com>
2017-04-26 20:16:33 -0700

    New font, radial layout and some bug fixes, WIP to merge other branches in

---
+++ Assets/testerino.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testerino : MonoBehaviour {
+
+    public Text t;
+	// Use this for initialization
+	void Start () {
+        t.text = "{ X";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
